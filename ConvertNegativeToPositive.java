8d5a412ef8a63f7a32a65f7031070ba412873c0c
Reenachristy <93434752+Reenachristy@users.noreply.github.com>
2021-10-31 08:29:52 +0530

    Add files via upload

---
+++ ConvertNegativeToPositive.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class ConvertNegativeToPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number= - 40;
+		if (number<=0) {
+			
+			System.out.println("The number -40 is converted to 40");
+			
+			
+		}
+		
+
+	}
+
+}
